11e161d1394303b7d94a0bce0617ebcfb1721bbd
Gileade Kelvin <gileadekelvin@gmail.com>
2023-04-23 21:05:59 -0300

    feat: config shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
-    <main className='flex h-screen items-center justify-center'>
+    <main className='flex h-screen flex-col items-center justify-center gap-4'>
       <h1 className='text-5xl font-bold'>Weather tree</h1>
+      <Button>Button</Button>
     </main>
   );
 }
